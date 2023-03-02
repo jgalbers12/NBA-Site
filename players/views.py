62ebf945b9d7e1409eb63affb13bcdb53ce17554
jgalbers <jgalbers12@gmail.com>
2023-03-01 19:44:08 -0600

    added league leaders

--- players/views.py
+++ players/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from . import utils
 
-# Create your views here.
+from . import utils
+from .forms import LeadersForm
+
 
 def player_info(request, id):
     info = utils.PlayerInfo(id)
@@ -12,3 +13,21 @@ def player_info(request, id):
         'stats': stats
     }
     return render(request, 'players/player_info.html', context=context)
+
+def player_leaders(request):
+    if request.GET:
+        params = request.GET
+        kwargs = {key:params[key] for key in request.GET if key != 'csrfmiddlewaretoken'} # replace this
+        box = utils.Leaders(**kwargs) # requires form fields align with __init__ params
+        form = LeadersForm(initial=kwargs)
+    else:
+        box = utils.Leaders()
+        form = LeadersForm()
+    stats = box.data
+    cols = stats[0].keys()
+    context = {
+        'stats': stats,
+        'cols': cols,
+        'form': form,
+    }
+    return render(request, 'players/leaders.html', context=context)
